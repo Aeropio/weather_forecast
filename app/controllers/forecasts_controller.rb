4258a1281f547bb025d27774f0c37fede2893815
EC2 Default User <ec2-user@ip-172-31-8-133.us-east-2.compute.internal>
2023-10-13 14:11:11 +0000

    added forecasts controller

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -4,12 +4,12 @@ class ForecastsController < ApplicationController
     @is_weather_cached = Rails.cache.exist?(@weather_cache_key)
     
     @weather = Rails.cache.fetch(@weather_cache_key, expires_in: 30.minutes) do
-        weather = WeatherService.new.execute(params[:zip_code], params[:country_code])
-        weather if weather.is_a?(OpenStruct) # Only cache if it's a Weather object
+      weather = WeatherService.new.execute(params[:zip_code], params[:country_code])
+      weather if weather.is_a?(OpenStruct) # Only cache if it's a Weather object
     end
    
     if @weather
-      render json: { data: weather_hash }, status: :ok
+      render json: { data: serialized_weather_data }, status: :ok
     else
       render json: { errors: "Error fetching weather data" }, status: :internal_server_error
     end
@@ -17,8 +17,8 @@ class ForecastsController < ApplicationController
 
   private
 
-  def weather_hash
-    {
+  def serialized_weather_data
+    @weather_hash = {
       temperature: @weather.temperature,
       temperature_min: @weather.temperature_min,
       temperature_max: @weather.temperature_max,
