fbc11f2d1f0a588fa13c648349f7c8e5b55f3ea9
EC2 Default User <ec2-user@ip-172-31-8-133.us-east-2.compute.internal>
2023-10-14 08:47:36 +0000

    added flash to display weather info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-    rescue_from(Faraday::Error) do |e|
-        error = {}
-        error[:title] = 'FaradayError'
-        error[:detail] = e.message
-        render json: { errors: error }, status: e.response[:status]
-    end
 end
--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,31 +1,46 @@
 class ForecastsController < ApplicationController
+  rescue_from Faraday::Error, with: :handle_faraday_error
+
   def show
-    @weather_cache_key = "#{params[:zip_code]},#{params[:country_code]}"
-    @is_weather_cached = Rails.cache.exist?(@weather_cache_key)
-    
-    @weather = Rails.cache.fetch(@weather_cache_key, expires_in: 30.minutes) do
-      weather = WeatherService.new.execute(params[:zip_code], params[:country_code])
-      weather if weather.is_a?(OpenStruct) # Only cache if it's a Weather object
-    end
-   
-    if @weather
-      render json: { data: serialized_weather_data }, status: :ok
+    if params[:zip_code].present? && params[:country_code].present?
+      fetch_weather_data(params[:zip_code], params[:country_code])
+      flash[:notice] = weather_flash_message if @weather
     else
-      render json: { errors: "Error fetching weather data" }, status: :internal_server_error
+      render :show
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @weather, status: :ok }
     end
   end
 
   private
 
-  def serialized_weather_data
-    @weather_hash = {
-      temperature: @weather.temperature,
-      temperature_min: @weather.temperature_min,
-      temperature_max: @weather.temperature_max,
-      humidity: @weather.humidity,
-      pressure: @weather.pressure,
-      description: @weather.description,
-      is_weather_cached: @is_weather_cached
-    }
+  def handle_faraday_error(e)
+    flash[:alert] = "Error fetching weather data: #{e.message}"
+    redirect_to root_path
+  end
+
+  def weather_flash_message
+    <<~MESSAGE
+      Temperature: #{@weather.temperature} ℃
+      Temperature Minimum: #{@weather.temperature_min} ℃
+      Temperature Maximum: #{@weather.temperature_max} ℃
+      Humidity: #{@weather.humidity}%
+      Pressure: #{@weather.pressure} millibars
+      Description: #{@weather.description}
+      Is this weather result from cache? #{@is_weather_cached}
+    MESSAGE
+  end
+
+  def fetch_weather_data(zip_code, country_code)
+    @weather_cache_key = "#{zip_code},#{country_code}"
+    @is_weather_cached = Rails.cache.exist?(@weather_cache_key)
+
+    @weather = Rails.cache.fetch(@weather_cache_key, expires_in: 30.minutes) do
+      weather = WeatherService.new.execute(zip_code, country_code)
+      weather if weather.is_a?(OpenStruct) # Only cache if it's a Weather object
+    end
   end
 end
